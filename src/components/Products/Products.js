3f68505533c4ad919392b1ee1c2094613da60707
graziela-queiroz <graziela-queiroz@hotmail.com>
2022-03-30 21:13:23 -0300

    card produtos

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,38 +1,94 @@
-import React from "react";
-import ProductCard from "../productCard/productCard";
+import React from 'react';
+import ProductCard from '../ProductCard/ProductCard';
+import styled from 'styled-components';
+
+const ProductCardDiv = styled.div`
+  display: flex,
+  flex-direction: row;
+`
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 16px;
+`
+const CardsProducts = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16px;
+  padding: 16px;
+`
 
 class Products extends React.Component {
+    state = {
+        products: [
+          {
+            id: 1,
+            name: "Produto 1",
+            price: 100,
+            photo: 'https://picsum.photos/200/200'
+          },
+          {
+            id: 2,
+            name: "Produto 2",
+            price: 100,
+            photo: 'https://picsum.photos/200/200'
+          },
+          {
+            id: 3,
+            name: "Produto 3",
+            price: 100,
+            photo: 'https://picsum.photos/200/200'
+          },
+          {
+            id: 4,
+            name: "Produto 4",
+            price: 100,
+            photo: 'https://picsum.photos/200/200'
+          },
+        ],
+
+      }
 
     getFilteredList = () => {
-        this.props.products
-            .filter((obj) => obj.price < maxValue)
-            .filter((obj) => obj.price > minValue)
-            .filter((obj) => obj.name.includes(this.props.nameValue))
+        // this.props.products
+        //     .filter((obj) => obj.price < maxValue)
+        //     .filter((obj) => obj.price > minValue)
+        //     .filter((obj) => obj.name.includes(this.props.nameValue))
             // .sort((a, b) => )
-
     }
 
     render(){
-        const filteredList = this.getFilteredList();
+        // const filteredList = this.getFilteredList();
+
+        const componentsProduct = this.state.products.map((prod) => {
+            return (
+              <ProductCard
+                key={prod.id}
+                name={prod.name}
+                price={prod.price}
+                photo={prod.photo}
+              />
+            );
+          });
+
         return(
-            <div className={"container-products"}>
-                <header className={"header-products"}>
-                    <p>Quantidade de produtos: 4</p>
-                    <label>
-                        Ordenação:
+            <ProductCardDiv>
+                <Header>
+                    <p>Quantidade produtos: {this.state.products.length}</p>
+                    <p>
+                        Ordenação
                         <select>
                             <option>Crescente</option>
                             <option>Decrescente</option>
                         </select>
-                    </label>
-                </header>
-                <div className={"product-box"}>
-                    {filteredList.map((obj) => {
-                        return <ProductCard product={obj}/>
-                    })}
-                </div>
+                    </p>
+                </Header>
 
-            </div>
+                <CardsProducts>
+                    {componentsProduct}
+                </CardsProducts>
+            </ProductCardDiv>
         )
     }
 }
